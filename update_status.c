82d69c2405afbc260a464016283ea74ba8563a20
Ludovico Fischer <livrerie@gmail.com>
2009-06-24 13:27:40 +0200

    update_status now adds 'status=' before message.

--- update_status.c
+++ update_status.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "update_status.h"
 #include "access_url.h"
 #include "get_credentials.h"
 #include "constants.h"
 
 int update_status(char *message) {
-    char username_password[80];
-    get_credentials(username_password, 81);
-    access_url(username_password, "http://twitter.com/statuses/update.xml", message, DO_POST);
+    char *status_string;
+    char *status_prefix = "status=";
+    status_string = (char*) calloc(strlen(message) + strlen(status_prefix), sizeof(char));
+    strcat(status_string, status_prefix);
+    strcat(status_string, message);
+    char username_password[USER_PASSWORD_SIZE];
+    get_credentials(username_password, USER_PASSWORD_SIZE + 1);
+    access_url(username_password, "http://twitter.com/statuses/update.xml", status_string, DO_POST);
+    free(status_string);
     printf("Updating status...\n");
     
     return 0;
