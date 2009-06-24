c3cf812330420dc01b9587c55fa04f2918830cff
Ludovico Fischer <livrerie@gmail.com>
2009-06-24 13:46:57 +0200

    strcat changed to strncat.

--- update_status.c
+++ update_status.c
@@ -10,8 +10,8 @@ int update_status(char *message) {
     char *status_string;
     char *status_prefix = "status=";
     status_string = (char*) calloc(strlen(message) + strlen(status_prefix), sizeof(char));
-    strcat(status_string, status_prefix);
-    strcat(status_string, message);
+    strncat(status_string, status_prefix, 7);
+    strncat(status_string, message, 300);
     char username_password[USER_PASSWORD_SIZE];
     get_credentials(username_password, USER_PASSWORD_SIZE + 1);
     access_url(username_password, "http://twitter.com/statuses/update.xml", status_string, DO_POST);
