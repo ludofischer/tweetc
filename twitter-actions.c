3da7d9f738a738ae1c8326bb00a900d3243b4661
Ludovico Fischer <livrerie@gmail.com>
2009-06-25 12:00:37 +0200

    Alert if status update exceeds maximum characters.

--- twitter-actions.c
+++ twitter-actions.c
@@ -21,15 +21,22 @@ int
 update_status(char *message) {
     char username_password[USER_PASSWORD_SIZE];
     const char *status_prefix = STATUS_PREFIX;
-    char *encoded = url_encode(message);
-    char *status_string = calloc(strlen(encoded) + strlen(status_prefix) + 1, sizeof(char));
-    strncat(status_string, status_prefix, STATUS_PREFIX_LENGTH);
-    strncat(status_string, encoded, 420 - STATUS_PREFIX_LENGTH);
-    get_credentials(username_password, USER_PASSWORD_SIZE + 1);
-    access_url(username_password, "http://twitter.com/statuses/update.xml", status_string, DO_POST);
-    free(status_string);
-    free (encoded);
-    printf("Updating status...\n");
+    size_t message_excess;
+    if ((message_excess = strlen(message) - MAX_TWEET_LENGTH) > 0) {
+        fprintf(stderr,"Your message is %zd characters too long\n.", message_excess);
+        return 1;
+    } else { 
+        char *encoded = url_encode(message);
+        char *status_string = calloc(strlen(encoded) + strlen(status_prefix) + 1, sizeof(char));
+
+        strncat(status_string, status_prefix, STATUS_PREFIX_LENGTH);
+        strncat(status_string, encoded, 420 - STATUS_PREFIX_LENGTH);
+        get_credentials(username_password, USER_PASSWORD_SIZE + 1);
+        access_url(username_password, "http://twitter.com/statuses/update.xml", status_string, DO_POST);
+        free(status_string);
+        free (encoded);
+        printf("Updating status...\n");
     
-    return 0;
+        return 0;
+}
 }
--- twitter-actions.h
+++ twitter-actions.h
@@ -3,6 +3,7 @@
 
 #define STATUS_PREFIX_LENGTH 7
 #define STATUS_PREFIX "status="
+#define MAX_TWEET_LENGTH 140
 
 int update_status(char*);
 int get_friends_timeline(void);
