c86c94f5a28b0ad8a67bae23f74a9adf641de0cc
Ludovico Fischer <livrerie@gmail.com>
2009-07-02 17:28:10 +0200

    Parser flow and test file added.

--- tests/sax-parse-friends-timeline.c
+++ tests/sax-parse-friends-timeline.c
@@ -2,27 +2,35 @@
 #include <string.h>
 #include <libxml/parser.h>
 
-#define PRINT 1
+#define TEXT 1
+#define AUTHOR 3
 #define NO_PRINT 2
 
 static void
 my_start_element(void *ctx, const xmlChar *name, const xmlChar **attrs) {
     int *state = ctx;
-    if (
-        xmlStrEqual(name, (const xmlChar *)"text") 
-        || xmlStrEqual(name, (const xmlChar *)"name") 
-        || xmlStrEqual(name, (const xmlChar *) "created_at")) {
-    *state = PRINT;
+    if (xmlStrEqual(name, (const xmlChar *)"text"))  {
+        *state = TEXT;       
+}
+    if (xmlStrEqual(name, (const xmlChar *)"screen_name")) {
+        *state = AUTHOR;
     }
 }
 
 static void
 my_characters(void *ctx, const xmlChar *ch, int len) {
     int *state =  ctx;
-    if (*state == PRINT) { 
-        xmlChar *sub = xmlStrsub(ch, 0, len);
-        puts((char *) sub);
+    xmlChar *good_text;
+    if (*state != NO_PRINT) {
+        good_text = xmlStrsub(ch, 0, len);
+    }
+    if (*state == TEXT) { 
+        puts((char *) good_text);
     } 
+    if (*state == AUTHOR) {
+        puts((char *) good_text);
+        printf("\n");
+    }
 }
 
 static void
@@ -32,12 +40,17 @@ my_end_element(void *ctx, const xmlChar *name) {
 }
 
 int
-main(void) {
+main(int argc, char **argv) {
+    if (argc == 2) {
     int state = NO_PRINT;
     xmlSAXHandler handler = 
     { .characters = my_characters,
       .startElement = my_start_element,
       .endElement = my_end_element};
-    xmlSAXUserParseFile(&handler, &state, "test.xml");
+    xmlSAXUserParseFile(&handler, &state, argv[1]);
     return 0;
+    } else {
+        perror("Wrong number of arguments.");
+        return 1;
+    }
 }
